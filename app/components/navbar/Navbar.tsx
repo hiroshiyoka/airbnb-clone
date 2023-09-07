0cd0747db207f30e96c733170c31173912b81f77
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-09-08 01:29:12 +0800

    feat: create logo for navbar

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,12 +1,15 @@
 import Container from "../Container";
 import React from "react";
+import Logo from "./Logo";
 
 const Navbar = () => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
-          <div className="flex flex-row items-center justify-between gap-3 md:gap-0"></div>
+          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+            <Logo />
+          </div>
         </Container>
       </div>
     </div>
