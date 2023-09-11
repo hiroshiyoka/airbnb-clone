43e72c211f6e10b2fb452e9fef46cf0bad3f4bf3
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-09-12 00:41:52 +0800

    feat: add styling to modal.tsx and apply handleClose into button

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState } from "react";
-import { useEffect } from "react";
-import { useCallback } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { IoMdClose } from "react-icons/io";
 
 interface ModalProps {
   isOpen?: boolean;
@@ -66,7 +65,30 @@ const Modal: React.FC<ModalProps> = ({
     return null;
   }
 
-  return <div></div>;
+  return (
+    <>
+      <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-neutral-800/70">
+        <div className="relative w-full md:w-4/6 lg:w-3/6 xl:w-2/5 my-6 mx-auto h-full lg:h-auto md:h-auto">
+          <div
+            className={`translate duration-300 h-full ${
+              showModal ? "translate-y-0" : "translate-y-full"
+            } ${showModal ? "opacity-100" : "opacity-0"}`}
+          >
+            <div className="translate h-full lg:h-auto md:h-auto border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="flex items-center p-6 rounded-t justify-center relative border-b-[1px]">
+                <button
+                  onClick={handleClose}
+                  className="p-1 border-0 hover:opacity-70 transition absolute left-9"
+                >
+                  <IoMdClose size={18} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
