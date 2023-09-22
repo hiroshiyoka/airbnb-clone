daf233a486b4fa5ba5190f4b936b66b5c6e3f6bc
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-09-22 15:03:43 +0700

    feat: import Input props

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -47,7 +47,13 @@ const RegisterModal = () => {
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Welcome to Airbnb" subtitle="Create an account" />
-      <Input />
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+      />
     </div>
   );
 
