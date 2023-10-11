03c04aa2776c828757036245ff6f82814bfadb4d
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-10-11 12:25:34 +0700

    feat: create three categories

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -2,6 +2,28 @@
 
 import Container from "../Container";
 
+import { TbBeach } from "react-icons/tb";
+import { GiWindmill } from "react-icons/gi";
+import { MdOutlineVilla } from "react-icons/md";
+
+export const categories = [
+  {
+    label: "Beach",
+    icon: TbBeach,
+    description: "This property is close to the beach!",
+  },
+  {
+    label: "Windmills",
+    icon: GiWindmill,
+    description: "This property has windmills!",
+  },
+  {
+    label: "Modern",
+    icon: MdOutlineVilla,
+    description: "This property is modern!",
+  },
+];
+
 const Categories = () => {
   return (
     <Container>
