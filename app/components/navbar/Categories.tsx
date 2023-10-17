971ecc25f41219129c6909f00af5ad2fe3f1bacf
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-10-17 13:31:07 +0700

    feat: import category box component into Categories Container

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -5,6 +5,7 @@ import Container from "../Container";
 import { TbBeach } from "react-icons/tb";
 import { GiWindmill } from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
+import CategoryBox from "../CategoryBox";
 
 export const categories = [
   {
@@ -27,7 +28,16 @@ export const categories = [
 const Categories = () => {
   return (
     <Container>
-      <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto"></div>
+      <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+        {categories.map((item) => (
+          <CategoryBox
+            key={item.label}
+            label={item.label}
+            description={item.description}
+            icon={item.icon}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
