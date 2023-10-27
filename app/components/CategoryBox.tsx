0073040905d52178710547653bdff4fe5793e1b7
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-10-27 17:47:02 +0700

    feat: add styling for category box

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -9,7 +9,13 @@ interface CategoryBoxProps {
 }
 
 const CategoryBox: React.FC<CategoryBoxProps> = ({ icon, label, selected }) => {
-  return <div></div>;
+  return (
+    <div
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
+        selected ? "border-b-neutral-800" : "border-transparent"
+      } ${selected ? "text-neutral-800" : "text-neutral-500"}`}
+    ></div>
+  );
 };
 
 export default CategoryBox;
