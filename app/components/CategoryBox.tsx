c75bc252819c2e88669c87e039d16ddea9664e7c
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-10-28 01:40:28 +0700

    feat: add Icon props

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -8,13 +8,19 @@ interface CategoryBoxProps {
   selected?: boolean;
 }
 
-const CategoryBox: React.FC<CategoryBoxProps> = ({ icon, label, selected }) => {
+const CategoryBox: React.FC<CategoryBoxProps> = ({
+  icon: Icon,
+  label,
+  selected,
+}) => {
   return (
     <div
       className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
         selected ? "border-b-neutral-800" : "border-transparent"
       } ${selected ? "text-neutral-800" : "text-neutral-500"}`}
-    ></div>
+    >
+      <Icon size={26} />
+    </div>
   );
 };
 
