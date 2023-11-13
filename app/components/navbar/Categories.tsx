f0556f131e26b49707918b40e9e03aa2b5795f7b
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-11-14 01:17:26 +0700

    feat: fix categories error by replace description with selected using item.label

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -44,7 +44,7 @@ const Categories = () => {
           <CategoryBox
             key={item.label}
             label={item.label}
-            description={item.description}
+            selected={category === item.label}
             icon={item.icon}
           />
         ))}
