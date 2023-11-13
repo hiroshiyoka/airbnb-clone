71c5a6d97e718d84bdff370460efb0a5b5e5328e
hiroshiy <fadilahsyafaat2427@gmail.com>
2023-11-14 01:09:06 +0700

    feat: make categories show a selected option itself

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -6,6 +6,7 @@ import { TbBeach } from "react-icons/tb";
 import { GiWindmill } from "react-icons/gi";
 import { MdOutlineVilla } from "react-icons/md";
 import CategoryBox from "../CategoryBox";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export const categories = [
   {
@@ -26,6 +27,16 @@ export const categories = [
 ];
 
 const Categories = () => {
+  const params = useSearchParams();
+  const category = params?.get("category");
+  const pathname = usePathname();
+
+  const isMainPage = pathname === "/";
+
+  if (isMainPage) {
+    return null;
+  }
+
   return (
     <Container>
       <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
