34ea76482b557ca746bf28520aa2c5186f40253b
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-01-01 01:24:52 +0700

    feat: create props for category input component

--- app/components/inputs/CategoryInput.tsx
+++ app/components/inputs/CategoryInput.tsx
@@ -1,6 +1,20 @@
 "use client";
 
-const CategoryInput = () => {
+import { IconType } from "react-icons";
+
+interface CategoryInputProps {
+  icon: IconType;
+  label: string;
+  selected: boolean;
+  onClick: (value: string) => void;
+}
+
+const CategoryInput: React.FC<CategoryInputProps> = ({
+  icon: Icon,
+  label,
+  selected,
+  onClick,
+}) => {
   return <div></div>;
 };
 
