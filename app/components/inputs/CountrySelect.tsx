a1d686959bf61c4aa450fa9d85216dfad8a053c8
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-01-09 18:34:41 +0700

    feat: import select from react-select and create type value for CountrySelect

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -1,5 +1,15 @@
 "use client";
 
+import Select from "react-select";
+
+export type CountrySelectValue = {
+  flag: string;
+  label: string;
+  latlng: string;
+  region: string;
+  value: string;
+};
+
 const CountrySelect = () => {
   return <div></div>;
 };
