6560abd4b5721d5a44f8c09bfe606c34bd1e17b4
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-01-10 21:41:03 +0700

    feat: add location value into CountrySelect component

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -45,6 +45,7 @@ const RentModal = () => {
   });
 
   const category = watch("category");
+  const location = watch("location");
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -107,7 +108,10 @@ const RentModal = () => {
           title="Where is your place located?"
           subtitle="Help guests find you!"
         />
-        <CountrySelect />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue("location", value)}
+        />
       </div>
     );
   }
