134bc721915856f2975655940a8d5d5303a68b5e
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-01-11 08:06:24 +0700

    feat: create a delete function

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -8,6 +8,14 @@ import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
 
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
+
 const Map = () => {
   return <div></div>;
 };
