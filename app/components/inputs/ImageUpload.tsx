30b1809d4d978644e7ba56e0b8a07b3ee50ba763
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-01-26 01:20:06 +0700

    feat: add TbPhotoPlus component for upload an image/photo

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -35,7 +35,9 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
           <div
             onClick={() => open?.()}
             className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
-          ></div>
+          >
+            <TbPhotoPlus size={50} />
+          </div>
         );
       }}
     </CldUploadWidget>
