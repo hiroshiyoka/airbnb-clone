677227f6db6ae13aa4d84b4c1f5131829580120c
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-02 03:04:27 +0700

    feat: create the EmptyState component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
   const isEmpty = true;
