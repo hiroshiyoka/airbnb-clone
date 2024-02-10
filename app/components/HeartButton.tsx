93962a67c6783c747ffdda738538eff8b885a8d1
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-11 06:51:57 +0700

    feat: create interface for heart button props

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { SafeUser } from "../types";
+
+interface HeartButtonProps {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
 const HeartButton = () => {
   return <div></div>;
 };
