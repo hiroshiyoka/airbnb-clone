6dc2c517ce6c8ca9caffe668395ac69ebc3ea915
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-10 20:35:05 +0700

    feat: add styling for the listing card component

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -6,6 +6,7 @@ import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
 import { format } from "date-fns";
+import Image from "next/image";
 
 interface ListingCardProps {
   data: Listing;
@@ -63,7 +64,29 @@ const ListingCard: React.FC<ListingCardProps> = ({
     return `${format(start, "PP")} - ${format(end, "PP")}`;
   }, [reservation]);
 
-  return <div>Listing Card</div>;
+  return (
+    <div
+      onClick={() => router.push(`/listings/${data.id}`)}
+      className="col-span-1 cursor-pointer group"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <div className="aspect-square w-full relative overflow-hidden rounded-xl">
+          <Image
+            fill
+            alt="Listing"
+            src={data.imageSrc}
+            className="
+              object-cover
+              h-full
+              w-full
+              group-hover:scale-110
+              transition
+            "
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ListingCard;
