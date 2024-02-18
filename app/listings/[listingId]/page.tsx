42d997274033b6998b38a77bfaec85dbd4b347a9
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-18 07:23:08 +0700

    feat: add the empty state if there is no listing

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,4 +1,6 @@
 import getListingById from "@/app/actions/getListingById";
+import ClientOnly from "@/app/components/ClientOnly";
+import EmptyState from "@/app/components/EmptyState";
 
 interface IParams {
   listingId?: string;
@@ -6,6 +8,14 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
+
+  if (!listing) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    );
+  }
 
   return <div></div>;
 };
