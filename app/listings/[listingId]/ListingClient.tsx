aa293ca6452084ccb12e405839870ac44ad298fe
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-20 07:23:15 +0700

    feat: create inteface props for listing client component

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,3 +1,14 @@
+import { SafeListing, SafeUser } from "@/app/types";
+import { Reservation } from "@prisma/client";
+
+interface ListingClientProps {
+  reservation?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser;
+  };
+  currentUser: SafeUser | null;
+}
+
 const ListingClient = () => {
   return <div>Listing Client</div>;
 };
