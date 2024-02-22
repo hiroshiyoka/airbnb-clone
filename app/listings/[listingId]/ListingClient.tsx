a3586b1c9f1395a2d9af21ac6b5e6af94ee23a85
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-22 10:23:16 +0700

    feat: import the listing head component into listing client component

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/listings/ListingHead";
 import { categories } from "@/app/components/navbar/Categories";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
@@ -25,7 +26,15 @@ const ListingClient: React.FC<ListingClientProps> = ({
   return (
     <Container>
       <div className="max-w-screen-lg mx-auto">
-        <div className="flex flex-col gap-6"></div>
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            locationValue={listing.locationValue}
+            imageSrc={listing.imageSrc}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+        </div>
       </div>
     </Container>
   );
