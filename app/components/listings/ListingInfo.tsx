067cd007335fab1ababa9d8f804428c7ea4e7349
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-23 07:38:01 +0700

    feat: create listing info component for property explanation

---
+++ app/components/listings/ListingInfo.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { SafeUser } from "@/app/types";
+import { IconType } from "react-icons";
+
+interface ListingInfoProps {
+  user: SafeUser;
+  description: string;
+  guestCount: number;
+  roomCount: number;
+  bathroomCount: number;
+  category:
+    | {
+        icon: IconType;
+        label: string;
+        description: string;
+      }
+    | undefined;
+  locationValue: string;
+}
+
+const ListingInfo: React.FC<ListingInfoProps> = ({
+  user,
+  description,
+  guestCount,
+  roomCount,
+  bathroomCount,
+  category,
+  locationValue,
+}) => {
+  return <div>Listing Info</div>;
+};
+
+export default ListingInfo;
