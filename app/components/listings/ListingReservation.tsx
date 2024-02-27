bd786223a2861b5b9b83d8990b4385e3cce71114
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-27 07:42:24 +0700

    feat: add styling for the listing reservation component

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -21,7 +21,27 @@ const ListingReservation: React.FC<ListingReservationProps> = ({
   disabled,
   disabledDates,
 }) => {
-  return <div></div>;
+  return (
+    <div
+      className="
+        bg-white
+        rounded-xl
+        border-[1px]
+        border-neutral-200
+        overflow-hidden
+      "
+    >
+      <div
+        className="
+        flex flex-row items-center gap-1 p-4
+      "
+      >
+        <div className="text-2xl font-semibold">$ {price}</div>
+        <div className="font-light text-neutral-600">night</div>
+      </div>
+      <hr />
+    </div>
+  );
 };
 
 export default ListingReservation;
