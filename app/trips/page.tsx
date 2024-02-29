b63ee638560a888e4c9b664cafa22d0731fed2ed
hiroshiy <fadilahsyafaat2427@gmail.com>
2024-02-29 10:06:00 +0700

    feat: create new page for trips route

---
+++ app/trips/page.tsx
@@ -0,0 +1,21 @@
+import EmptyState from "../components/EmptyState";
+import ClientOnly from "../components/ClientOnly";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservation from "../actions/getReservation";
+
+const TripsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservation = await getReservation({
+    userId: currentUser.id,
+  });
+};
