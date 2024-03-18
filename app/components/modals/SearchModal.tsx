adc312636634dee1eb686662071fd0652fa91cfb
hiroshiyoka <fadilahsyafaat2427@gmail.com>
2024-03-19 00:31:40 +0700

    feat(modal): expand SearchModal functionality and UI

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -1,11 +1,45 @@
 "use client";
 
+import { useParams, useRouter } from "next/navigation";
+import { useMemo, useState } from "react";
+import { Range } from "react-date-range";
+
 import Modal from "./Modal";
+import { CountrySelectValue } from "../inputs/CountrySelect";
 
 import useSearchModal from "@/app/hooks/useSearchModal";
+import dynamic from "next/dynamic";
+
+enum STEPS {
+  LOCATION = 0,
+  DATE = 1,
+  INFO = 2,
+}
 
 const SearchModal = () => {
+  const router = useRouter();
+  const params = useParams();
   const searchModal = useSearchModal();
+
+  const [location, setLocation] = useState<CountrySelectValue>();
+  const [step, setStep] = useState(STEPS.LOCATION);
+  const [guestCount, setGuestCount] = useState(1);
+  const [roomCount, setRoomCount] = useState(1);
+  const [bathroomCount, setBathroomCount] = useState(1);
+  const [dateRange, setDateRange] = useState<Range>({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
+
   return (
     <Modal
       isOpen={searchModal.isOpen}
